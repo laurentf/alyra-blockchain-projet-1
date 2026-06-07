8d4c464cf1b717c90b07f4f7994c3521c976eb2e
laurentf <laurentf.bonnet@gmail.com>
2026-06-07 18:06:21 +0200

    feat: add voter whitelist registration

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -39,4 +39,22 @@ contract Voting is Ownable {
     event Voted(address voter, uint proposalId);
 
     constructor() Ownable(msg.sender) {}
+
+    function registerVoter(address _voterAddress) external onlyOwner {
+        // Only the owner can register voters, and it must be during the RegisteringVoters phase
+        require(
+            currentWorkflowStatus == WorkflowStatus.RegisteringVoters,
+            "Voters registration is not open"
+        );
+        // A voter cannot be registered more than once
+        require(
+            !voters[_voterAddress].isRegistered,
+            "Voter is already registered"
+        );
+
+        // Register the voter
+        voters[_voterAddress] = Voter(true, false, 0);
+        // Emit an event when a voter is registered
+        emit VoterRegistered(_voterAddress);
+    }
 }
