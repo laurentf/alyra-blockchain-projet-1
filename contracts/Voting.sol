c9e0f303a785e71762c2a8a761665f22b7d89572
laurentf <laurentf.bonnet@gmail.com>
2026-06-07 17:57:29 +0200

    feat: add core data structures, events and Ownable admin

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,6 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.35;
+pragma solidity 0.8.34;
 
-contract Voting {
+// v5 so need constructor with Ownable(msg.sender) in constructor
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract Voting is Ownable {
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    WorkflowStatus public currentWorkflowStatus; // initialized to RegisteringVoters by default
+    mapping(address => Voter) public voters;
+    Proposal[] public proposals;
+    uint public winningProposalId;
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint proposalId);
+    event Voted(address voter, uint proposalId);
+
+    constructor() Ownable(msg.sender) {}
 }
